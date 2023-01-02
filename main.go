9bc3e9237e3b55d1c4c01a67a7ebe58edd81ed59
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2023-01-02 19:29:14 +0100

    crud func

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-import (
+import(
 	"fmt"
 	"log"
+	"encoding/json"
+	"math/random"
 	"net/http"
-
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -22,6 +24,34 @@ type Director struct{
 }
 
 var movies []Movie
+
+func getMovies (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[:index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
 
 func main(){
 	r := mux.NewRouter()
