e0df9760ce9ef479dd1e836e731fbdceb6f4d064
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2023-01-02 18:49:03 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct{
 	ID string `json:"id"`
 	Isbn string `json:"isbn"`
@@ -13,6 +21,18 @@ type Director struct{
 	LastName string `json:"lastname"`
 }
 
+var movies []Movie
+
 func main(){
-	
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "123456", Title: "Pulp Fiction", Director: &Director{FirstName: "Quintin", LastName: "Tarantino"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "123457", Title: "The Grand Budapest Hotel", Director: &Director{FirstName: "Wes", LastName: "Anderson"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	fmt.Printf("Starting Server at port 800")
+	log.Fatal(http.ListenAndServe("8000", r))
 }
